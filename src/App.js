8179392c88cec0f717311cf2415ec23ba399eb63
Tchaikousky Thomas <tchaikouskythomas@gmail.com>
2020-03-24 11:09:44 -0400

    created title/subtitle with subtle styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 id="header-title">Memory Game</h1>
+        <h3 id="header-subtitle">
+          Match cards to win.
+        </h3>
       </header>
     </div>
   );
